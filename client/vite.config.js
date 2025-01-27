1a11121726b2347cc7681870342f757f51913f1c
mariana <marianavaladares@gmail.com>
2025-01-27 11:17:04 +0000

    Updated vite and vercel file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
+      '@src': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
+    },
+  },
   build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+    rollupOptions: {
+      external: ['react-hot-toast/dist/react-hot-toast.esm.js'],
+    },
+  },
+  optimizeDeps: {
+    include: ['react-hot-toast'],
+  },
 });
